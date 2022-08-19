65975f77b1f042c7843f6005ead4581a302b71d8
Joseph Timbol <joseph2000k@gmail.com>
2022-08-19 13:38:27 +0800

    Category table created

--- client/src/pages/Items.tsx
+++ client/src/pages/Items.tsx
@@ -1,53 +1,52 @@
-import React, {useState} from 'react';
-import { useQuery } from '@apollo/client';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import ItemTable from '../components/ItemTable';
-import CreateItem from '../components/CreateItem';
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import ItemTable from "../components/ItemTable";
+import CreateItem from "../components/CreateItem";
 
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import {GET_CATEGORIES} from '../graphql/query/CategoryQuery';
-import { GET_ALL_ITEMS, GET_ITEMS_BY_CATEGORY } from '../graphql/query/ItemQuery';
-import ProgressBar from '../components/ProgressBar';
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { GET_CATEGORIES } from "../graphql/query/CategoryQuery";
+import {
+  GET_ALL_ITEMS,
+  GET_ITEMS_BY_CATEGORY,
+} from "../graphql/query/ItemQuery";
+import ProgressBar from "../components/ProgressBar";
 //for Dial
-import SpeedDial from '@mui/material/SpeedDial';
-import SpeedDialIcon from '@mui/material/SpeedDialIcon';
-import Tooltip from '@mui/material/Tooltip';
-import CreateIcon from '@mui/icons-material/Create';
+import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
+import Tooltip from "@mui/material/Tooltip";
+import CreateIcon from "@mui/icons-material/Create";
 //for Modal
-import Modal from '@mui/material/Modal';
-import Backdrop from '@mui/material/Backdrop';
-import Fade from '@mui/material/Fade';
+import Modal from "@mui/material/Modal";
+import Backdrop from "@mui/material/Backdrop";
+import Fade from "@mui/material/Fade";
 
-
-
-
-function Items(){
-
+function Items() {
   //Modal Transition
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const [category, setCategory] = useState('All Items');
+  const [category, setCategory] = useState("All Items");
 
   const { loading, error, data } = useQuery(GET_CATEGORIES);
 
   const getItems = useQuery(GET_ALL_ITEMS);
   const getItemsByCategory = useQuery(GET_ITEMS_BY_CATEGORY, {
     variables: {
-      categoryId: category
-    }
+      categoryId: category,
+    },
   });
 
- let items = [];
-  if(category === 'All Items' && !getItems.loading && !getItems.error){
+  let items = [];
+  if (category === "All Items" && !getItems.loading && !getItems.error) {
     items = getItems.data.getItems;
-  } else if(!getItemsByCategory.loading && !getItemsByCategory.error){
+  } else if (!getItemsByCategory.loading && !getItemsByCategory.error) {
     items = getItemsByCategory.data.getItemsByCategory;
   }
 
@@ -60,115 +59,125 @@ function Items(){
 
   //Modal Style
   const style = {
-    position: 'absolute' as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
     width: 400,
-    bgcolor: 'background.paper',
+    bgcolor: "background.paper",
     borderRadius: 1,
     boxShadow: 24,
     p: 4,
   };
 
-    return (
-      <>
+  return (
+    <>
       {!loading && !error && (
         <Container>
-        <Box sx={
-          {
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: '20px',
-          }
-        }>
-      <div>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: "center", maxWidth: '100%', marginBottom: 2}}>
-        <Box>
-      <Typography
-          sx={{ flex: '1 1 100%' }}
-          variant="h5"
-          id="tableTitle"
-          component="div"
-        >
-          
-          Items
-          </Typography>
-          </Box>
-          <Box sx={{width: 200}}>
-          <FormControl fullWidth>
-  <InputLabel id="category-select-label">Categories</InputLabel>
-  <Select
-    labelId="category-label"
-    id="category-select"
-    value={category}
-    label="Categories"
-    onChange={handleChange}
-    defaultValue="All Items"
-  >
-    <MenuItem value="All Items">All Items</MenuItem>
-     {
-      data.getCategory.map((category: any) => (
-        <MenuItem key={category._id} value={category._id}>{category.categoryName}</MenuItem>
-      ))
-     }
-  </Select>
-</FormControl>
-</Box>
-          </Box>
-          
-      {
-        <ItemTable items = {items}/>
-      }
-      </div>
-      
-      </Box>
-      
-      </Container>
-      )}
-      <Box sx={{ 
-          position: 'fixed',
-          bottom: 0,
-          right: '1%',
-          zIndex: 'modal', }}>
-      <Tooltip title="Create Item" placement="top" TransitionComponent={Fade}
-  TransitionProps={{ timeout: 600 }}>
-      <SpeedDial
-        ariaLabel="SpeedDial"
-        sx={{ position: 'absolute', bottom: 16, right: 20 }}
-        icon={<SpeedDialIcon openIcon={<CreateIcon />} />}
-        onClick={handleOpen}
-      >
-      </SpeedDial>
-      </Tooltip>
-    </Box>
-      
+          <Box
+            sx={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: "20px",
+            }}
+          >
+            <div>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  maxWidth: "100%",
+                  marginBottom: 2,
+                }}
+              >
+                <Box>
+                  <Typography
+                    sx={{ flex: "1 1 100%" }}
+                    variant="h5"
+                    id="tableTitle"
+                    component="div"
+                  >
+                    Items
+                  </Typography>
+                </Box>
+                <Box sx={{ width: 200 }}>
+                  <FormControl fullWidth>
+                    <InputLabel id="category-select-label">
+                      Categories
+                    </InputLabel>
+                    <Select
+                      labelId="category-label"
+                      id="category-select"
+                      value={category}
+                      label="Categories"
+                      onChange={handleChange}
+                      defaultValue="All Items"
+                    >
+                      <MenuItem value="All Items">All Items</MenuItem>
+                      {data.getCategory.map((category: any) => (
+                        <MenuItem key={category._id} value={category._id}>
+                          {category.categoryName}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </Box>
+              </Box>
 
-    <div>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
+              {<ItemTable items={items} />}
+            </div>
+          </Box>
+        </Container>
+      )}
+      <Box
+        sx={{
+          position: "fixed",
+          bottom: 0,
+          right: "1%",
+          zIndex: "modal",
         }}
       >
-        <Fade in={open}>
-          <Box sx={style}>
-            <CreateItem displayedCategory={category}/>
-          </Box>
-        </Fade>
-      </Modal>
-    </div>
-      </>
-    );
-  }
-  
-  export default Items
+        <Tooltip
+          title="Create Item"
+          placement="top"
+          TransitionComponent={Fade}
+          TransitionProps={{ timeout: 600 }}
+        >
+          <SpeedDial
+            ariaLabel="SpeedDial"
+            sx={{ position: "absolute", bottom: 16, right: 20 }}
+            icon={<SpeedDialIcon openIcon={<CreateIcon />} />}
+            onClick={handleOpen}
+          ></SpeedDial>
+        </Tooltip>
+      </Box>
+
+      <div>
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <Box sx={style}>
+              <CreateItem displayedCategory={category} />
+            </Box>
+          </Fade>
+        </Modal>
+      </div>
+    </>
+  );
+}
+
+export default Items;
