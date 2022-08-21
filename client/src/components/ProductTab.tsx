156d8dd914802c35c68d03aa5d9ed7f63b3f7571
Joseph Timbol <joseph2000k@gmail.com>
2022-08-21 22:03:37 +0800

    category tab mapping

--- client/src/components/ProductTab.tsx
+++ client/src/components/ProductTab.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
+import { GET_CATEGORIES } from "../graphql/query/CategoryQuery";
+import { useQuery } from "@apollo/client";
+import ProgressBar from "../components/ProgressBar";
 
 export default function ProductTab() {
   const [value, setValue] = React.useState(0);
@@ -10,18 +13,30 @@ export default function ProductTab() {
     setValue(newValue);
   };
 
+  const { loading, error, data } = useQuery(GET_CATEGORIES);
+
+  if (loading) return <ProgressBar />;
+
+  function createData(_id: string, categoryName: string) {
+    return { _id, categoryName };
+  }
+
+  const categories = data.getCategory.map((category: any) => {
+    return createData(category._id, category.categoryName);
+  });
+
   return (
     <Box sx={{ maxWidth: { xs: 600, sm: 1000, md:1500, lg: 1500} }}>
       <Tabs
         value={value}
         onChange={handleChange}
-        variant="scrollable"
+        variant = "scrollable"
         scrollButtons="auto"
         aria-label=""
       >
-        <Tab label="Washi Tapes" />
-        <Tab label="Stickers" />
-        <Tab label="Wooden Stamps" />
+        {categories.map((row: any) => (
+          <Tab key={row._id} label={row.categoryName} />
+        ))}
       </Tabs>
     </Box>
   );
