ff72264640d42fc05d897461c75b3701696150a6
Joseph Timbol <joseph2000k@gmail.com>
2022-08-25 16:20:08 +0800

    POS items preparation

--- client/src/components/PosItems.tsx
+++ client/src/components/PosItems.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import ProgressBar from "./ProgressBar";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
 
-export default function PosItems() {
+export default function PosItems({ items: items }: any) {
+  console.log(items);
+
+  if (items.length === 0) {
+    return <ProgressBar />;
+  }
+
   return (
-    <div>
-      Items here
-    </div>
+    <ImageList sx={{ maxHeight: "100px" }}>
+      {items.map((item: any) => (
+        <ImageListItem key={item._id}>
+          <img
+            src={`images/${item.image}`}
+            alt={item.name}
+            width="75"
+            height="75"
+          />
+
+          <ImageListItemBar position="below" />
+        </ImageListItem>
+      ))}
+    </ImageList>
   );
 }
--- client/src/pages/POS.tsx
+++ client/src/pages/POS.tsx
@@ -1,76 +1,71 @@
-import ProductTab from "../components/ProductTab";
+import Products from "../components/Products";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
-import PosItems from "../components/PosItems";
 import PosReceipt from "../components/PosReceipt";
 import CssBaseline from "@mui/material/CssBaseline";
-import { styled } from '@mui/material/styles';
+import { styled } from "@mui/material/styles";
 
-const Receipt = styled('div')(({ theme }) => ({
-  [theme.breakpoints.up('sm')]: {
+const Receipt = styled("div")(({ theme }) => ({
+  [theme.breakpoints.up("sm")]: {
     marginLeft: "210px",
   },
-  [theme.breakpoints.up('md')]: {
+  [theme.breakpoints.up("md")]: {
     marginLeft: "120px",
   },
-  [theme.breakpoints.up('lg')]: {
+  [theme.breakpoints.up("lg")]: {
     marginLeft: "215px",
   },
 }));
 
-const Items = styled('div')(({ theme }) => ({
-
-  [theme.breakpoints.up('sm')]: {
+const Items = styled("div")(({ theme }) => ({
+  [theme.breakpoints.up("sm")]: {
     width: "550px",
   },
-  [theme.breakpoints.up('md')]: {
+  [theme.breakpoints.up("md")]: {
     width: "800px",
   },
-  [theme.breakpoints.up('lg')]: {
+  [theme.breakpoints.up("lg")]: {
     width: "1080px",
   },
 }));
-
-
 
 function POS() {
   return (
     <>
       <Grid style={{ marginTop: "60px" }}>
-        <Grid container component="main" >
+        <Grid container component="main">
           <CssBaseline />
-          <Grid item xs={false} sm={5} md={7} >
+          <Grid item xs={false} sm={5} md={7}>
             <Grid style={{ position: "sticky" }}>
               <Items>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                }}
-              >
-                <ProductTab />
-                <PosItems />
-              </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  <Products />
+                </Box>
               </Items>
             </Grid>
           </Grid>
           <Receipt>
-          <Grid
-            item
-            xs={12}
-            sm={4}
-            md={5}
-            component={Paper}
-            elevation={6}
-            square
-            maxWidth="800"
-            style={{ maxHeight: "100vh", overflow: "auto"}}
-            position="fixed"
-          >
-            <PosReceipt />
-          </Grid>
+            <Grid
+              item
+              xs={12}
+              sm={4}
+              md={5}
+              component={Paper}
+              elevation={6}
+              square
+              maxWidth="800"
+              style={{ maxHeight: "100vh", overflow: "auto" }}
+              position="fixed"
+            >
+              <PosReceipt />
+            </Grid>
           </Receipt>
         </Grid>
       </Grid>
