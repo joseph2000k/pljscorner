72a000877d24b6744d7f522092521bda13856980
Joseph Timbol <joseph2000k@gmail.com>
2022-09-09 10:19:23 +0800

    payment input

--- client/src/components/CreateReceipt.tsx
+++ client/src/components/CreateReceipt.tsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { PaymentContext } from "../context/paymentContext";
 import { ThemeProvider, useTheme } from "@mui/material/styles";
 import Avatar from "@mui/material/Avatar";
 import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Input from "@mui/material/Input";
+import InputAdornment from "@mui/material/InputAdornment";
+import PaymentIcon from "@mui/icons-material/Payment";
 
 export default function CreateReceipt({ setOpen }: any) {
+  const { total } = useContext(PaymentContext);
   const [isPayed, setIsPayed] = useState(false);
 
   const theme = useTheme();
@@ -13,9 +23,30 @@ export default function CreateReceipt({ setOpen }: any) {
   const toPay = (
     <>
       <Typography id="transition-modal-title" variant="h5" component="h2">
-        Amount Due: ₱ 0.00
+        Amount Due: ₱ {total}
       </Typography>
-      <button
+
+      <Box sx={{ display: "flex", alignItems: "center", mt: 3, mb: 2 }}>
+        <Grid container spacing={2}>
+          <FormControl fullWidth sx={{ m: 1 }} variant="standard">
+            <InputLabel htmlFor="standard-adornment-amount">
+              Cash Amount
+            </InputLabel>
+            <Input
+              autoFocus
+              type="number"
+              id="standard-adornment-amount"
+              startAdornment={
+                <InputAdornment position="start">₱</InputAdornment>
+              }
+            />
+          </FormControl>
+        </Grid>
+      </Box>
+
+      <Button
+        color="secondary"
+        variant="contained"
         type="submit"
         //onsubmit prevent default to prevent page refresh
         onClick={(e) => {
@@ -23,8 +54,9 @@ export default function CreateReceipt({ setOpen }: any) {
           setIsPayed(true);
         }}
       >
-        Pay
-      </button>
+        Pay &nbsp;
+        <PaymentIcon />
+      </Button>
     </>
   );
 
@@ -32,14 +64,17 @@ export default function CreateReceipt({ setOpen }: any) {
     <>
       <Typography id="transition-modal-title" variant="h5" component="h2">
         payed
-        <button
+        <Button
+          autoFocus
+          color="secondary"
+          variant="contained"
           onClick={(e) => {
             e.preventDefault();
             setOpen(false);
           }}
         >
           close
-        </button>
+        </Button>
       </Typography>
     </>
   );
