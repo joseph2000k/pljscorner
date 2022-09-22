0f78984cefc95a6bab9a855c1b4ae6e6a21bb178
Joseph Timbol <joseph2000k@gmail.com>
2022-09-22 11:25:59 +0800

    cart redesign

--- src/graphql/resolvers/cart.ts
+++ src/graphql/resolvers/cart.ts
@@ -57,22 +57,9 @@ module.exports = {
                 }
 
                 const cartItem = cart.items.find((item: { itemId: { toString: () => string; }; }) => item.itemId.toString() === args.cartInput);
-                const discount = await BMSDiscount.findOne({item: args.cartInput, activated: true});
-                const discount2 = await SMDiscount.findOne({items: args.cartInput, activated: true});
 
-          
-                //get total quantity of cart items that has discount property of discount2._id
-                let totalQuantityWithDiscount = 0;
-                cart.items.forEach((item: { discount: any; quantity: number; }) => {
-                    if(item.discount.length > 0) {
-                        totalQuantityWithDiscount += item.quantity;
-                    }
-                });
-
-
+                console.log(cartItem)
                 
-
-
                 if(!cartItem) {
                     cart.items.push({
                         itemId: item._id,
@@ -80,106 +67,9 @@ module.exports = {
                         quantity: 1,
                         price: item.price,
                     });
-                }else if(totalQuantityWithDiscount > 0) {
-                    //log if mulitiple of discount.buy
-                    if(totalQuantityWithDiscount % discount2.buy === 0) {
-                        cart.items.push({
-                        itemId: item._id,
-                        item: item.name,
-                        quantity: 1,
-                        price: item.price,
-                    });
-                    }
-
-                }else {
+                } else {
                     cartItem.quantity += 1;
                     cartItem.price += item.price;
-                }
-
-
-                let items=[];
-                if(discount2) {
-                    const itemsInDiscount2 = cart.items.filter((item: { itemId: { toString: () => string; }; }) => discount2.items.includes(item.itemId.toString()));
-                    items.push(...itemsInDiscount2);
-                }
-                
-                let totalQuantity = 0;
-                items.forEach((item: { quantity: number; }) => {
-                    totalQuantity += item.quantity;
-                });
-
-                if(discount2){
-                    let remainder = totalQuantity % discount2.buy;
-
-                    if(remainder===0) {
-                        cart.items.forEach((item: { itemId: { toString: () => string; }; discount: any; price: number }) => {
-                            if(discount2.items.includes(item.itemId.toString())) {
-                                /* item.price = item.price - discount2.saveValue; */
-                                item.discount.push(discount2._id);
-                            }
-                        });
-                        //find all items in cart that has discount2._id in discount property
-                        let toJoinItems: { itemId: { toString: () => string; }; discount: any; price: number; }[]=[];
-                        cart.items.forEach((item: { itemId: { toString: () => string; }; discount: any; price: number }) => {
-                            if(item.discount.includes(discount2._id)) {
-                                //store all found items in an array then push to cart.items
-                                toJoinItems.push(item);
-                            }
-                        });
-
-                        //find all itmes in toJoinItems that has quantity greater than 1 then separate them into individual items then push to cart.items\
-                        toJoinItems.forEach((item:any) => {
-                            if(item.quantity > 1) {
-                                for(let i = 0; i < item.quantity; i++) {
-                                    cart.items.push({
-                                        itemId: item.itemId,
-                                        item: item.item + discount2.title,
-                                        quantity: 1,
-                                        price: 0,
-                                        discount: item.discount
-                                    });
-                                    //if first index then change price to discount.saveValue
-                                }
-                            }
-                            //change price to 0
-                            item.price = 0;
-                        });
-                        //remove items in cartitems that has a discount property that has discount2._id with quantity greater than 1
-                        cart.items = cart.items.filter((item: {
-                            quantity: number; itemId: { toString: () => string; }; discount: any; price: number 
-}) => {
-                            if(item.discount.includes(discount2._id) && item.quantity > 1) {
-                                return false;
-                            }
-                            return true;
-                        });
-
-                        toJoinItems=[]
-
-                        //sort cart items that has discount2._id in discount property
-                cart.items = cart.items.sort((a: { discount: any; }, b: { discount: any; }) => {
-                    if(a.discount.includes(discount2._id) && !b.discount.includes(discount2._id)) {
-                        return -1;
-                    } else if(!a.discount.includes(discount2._id) && b.discount.includes(discount2._id)) {
-                        return 1;
-                    } else {
-                        return 0;
-                    }
-                });
-                    } 
-
-                    //select last index of cart.items that has discount2._id in discount property
-                    let lastIndex = cart.items.length - 1;
-                    for(let i = lastIndex; i >= 0; i--) {
-                        if(cart.items[i].discount.includes(discount2._id)) {
-                            lastIndex = i;
-                            cart.items[i].price = discount2.saveValue;
-                            break;
-                        }
-                    }
-
-                    //reverse cart.items
-                    cart.items = cart.items.reverse();
                 }
 
                 
@@ -199,8 +89,7 @@ module.exports = {
             try {
                 const cart = await Cart.findOne({user: user.id});
                 const item = await Item.findById(args.cartInput);
-                const discount = await BMSDiscount.findOne({item: args.cartInput, activated: true});
-                const discount2 = await SMDiscount.findOne({items: args.cartInput, activated: true});
+                
                 
                 if(!cart) {
                     return new Error('Cart not found');
@@ -214,15 +103,10 @@ module.exports = {
                
                 if(!cartItem) {
                     return new Error('Item not found in cart');
-                } /* else if(cartItem && discount) {
-                    const remainder = cartItem.quantity % discount.buy;
-                    if(remainder === 0) {
-                        cartItem.price -= item.price - discount.saveValue;
-                    } else {
+                } else if(cartItem) {
                     cartItem.price -= item.price;
-                    }
                     cartItem.quantity -= 1;
-                } */
+                }
                 else {
                     cartItem.quantity -= 1;
                     cartItem.price -= item.price;
@@ -232,19 +116,6 @@ module.exports = {
                     cart.items = cart.items.filter((item: { itemId: { toString: () => string; }; }) => item.itemId.toString() !== args.cartInput);
                 }
 
-                let totalQuantityOfItemsInDiscount = 0;
-                if(discount2) {
-                    const itemsInDiscount = cart.items.filter((item: { itemId: { toString: () => string; }; }) => discount2.items.includes(item.itemId.toString()));
-                    itemsInDiscount.forEach((item: { quantity: number; }) => {
-                        totalQuantityOfItemsInDiscount += item.quantity;
-                    });
-                }
-
-                let remainder = totalQuantityOfItemsInDiscount % discount2.buy;
-                if(remainder === 0) {
-                    console.log('remainder is 0');
-                }
-
                 item.stock += 1;
 
                 await item.save();
@@ -252,6 +123,7 @@ module.exports = {
                 await cart.save();
 
                 return cart;
+
 
             } catch (err) {
                 return err;
