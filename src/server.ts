c175a64d64dceca54d0597ff8b28328c0d00faff
Joseph Timbol <joseph2000k@gmail.com>
2022-09-30 12:05:14 +0800

    Prepared for deployment

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import express from 'express';
 import http from 'http';
 import { expressjwt, Request as JWtRequest } from 'express-jwt';
 import {permissions} from './permissions';
+const path = require('path');
 
 const { graphqlUploadExpress } = require("graphql-upload-minimal");
 const config = require('config');
@@ -51,8 +52,18 @@ async function startApolloServer(typeDefs: any, resolvers: any) {
   await server.start();
   server.applyMiddleware({ app });
 
+//serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+}
 //connect to Database
-const port = 5000
+const port = process.env.PORT || 5000
 connectDB()
   .then(() => {
     return httpServer.listen({ port });
