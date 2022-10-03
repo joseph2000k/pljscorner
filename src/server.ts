e0f6746c839368a90e5ef7cdf5ff9b83bc381759
Joseph Timbol <joseph2000k@gmail.com>
2022-10-03 15:15:36 +0800

    undo removing of production in server.ts

--- src/server.ts
+++ src/server.ts
@@ -52,6 +52,15 @@ async function startApolloServer(typeDefs: any, resolvers: any) {
   await server.start();
   server.applyMiddleware({ app });
 
+//serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 //connect to Database
 const port = process.env.PORT || 5000
