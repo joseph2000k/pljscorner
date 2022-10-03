ec8c7699e3957b157bcaa8c146f6f30379da0f38
Joseph Timbol <joseph2000k@gmail.com>
2022-10-03 16:35:27 +0800

    production removed

--- src/server.ts
+++ src/server.ts
@@ -53,14 +53,14 @@ async function startApolloServer(typeDefs: any, resolvers: any) {
   server.applyMiddleware({ app });
 
 //serve static assets in production
- if(process.env.NODE_ENV === 'production') {
+/*  if(process.env.NODE_ENV === 'production') {
   //set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-} 
+}  */
 
 //connect to Database
 const port = process.env.PORT || 5000
