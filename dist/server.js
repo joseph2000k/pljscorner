70aa68ae5d83427d4f32ac1e25b790712f02e475
Joseph Timbol <joseph2000k@gmail.com>
2022-10-06 11:10:01 +0800

    PORT changed in server.js in dist folder

--- dist/server.js
+++ dist/server.js
@@ -1,18 +1,18 @@
 "use strict";
-const { ApolloServer } = require('apollo-server');
-const connectDB = require('./config/db');
-const typeDefs = require('./graphql/typeDefs');
-const resolvers = require('./graphql/resolvers');
+const { ApolloServer } = require("apollo-server");
+const connectDB = require("./config/db");
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+  typeDefs,
+  resolvers,
 });
 //connect to Database
-const port = 5000;
+const PORT = process.env.PORT;
 connectDB()
-    .then(() => {
-    return server.listen({ port });
-})
-    .then(() => {
+  .then(() => {
+    return server.listen({ PORT });
+  })
+  .then(() => {
     console.log(`Server is running on port http://localhost:${port}/`);
-});
+  });
