71ad3bcc829960b472144dafcb1659ea1bc22ebf
Joseph Timbol <joseph2000k@gmail.com>
2022-10-08 16:34:04 +0800

    apollo client uri

--- client/src/apolloClient.tsx
+++ client/src/apolloClient.tsx
@@ -5,7 +5,7 @@ import { createUploadLink } from "apollo-upload-client";
 const port = process.env.PORT || 5000;
 
 const uploadLink = createUploadLink({
-  uri: `http://localhost:${port}/graphql`,
+  uri: `http://pljscorner.herokuapp.com/graphql`,
 });
 
 const authLink = setContext((_, { headers }) => {
